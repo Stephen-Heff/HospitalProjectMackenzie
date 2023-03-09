3e81a24172afd923962831007baf9f0463ee5bff
Stephen-Heff <folosstephen@gmail.com>
2023-03-09 14:25:08 -0500

    Add project files.

---
+++ HospitalProjectMackenzie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalProjectMackenzie.Startup))]
+namespace HospitalProjectMackenzie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
