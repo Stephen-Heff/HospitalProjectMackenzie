f34d1dde285612723928b71a381c840b60f76c82
Stephen-Heff <folosstephen@gmail.com>
2023-03-23 14:05:27 -0400

    cascade delete

---
+++ HospitalProjectMackenzie/Migrations/202303231804379_cascade.cs
@@ -0,0 +1,28 @@
+﻿namespace HospitalProjectMackenzie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class cascade : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Appointments", "DoctorID", "dbo.Appointments");
+            DropForeignKey("dbo.Appointments", "PatientID", "dbo.Appointments");
+            DropForeignKey("dbo.Appointments", "RoomID", "dbo.Appointments");
+            AddForeignKey("dbo.Appointments", "DoctorID", "dbo.Doctors", "DoctorID", cascadeDelete: true);
+            AddForeignKey("dbo.Appointments", "PatientID", "dbo.Patients", "PatientID", cascadeDelete: true);
+            AddForeignKey("dbo.Appointments", "RoomID", "dbo.Rooms", "RoomID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Appointments", "RoomID", "dbo.Rooms");
+            DropForeignKey("dbo.Appointments", "PatientID", "dbo.Patients");
+            DropForeignKey("dbo.Appointments", "DoctorID", "dbo.Doctors");
+            AddForeignKey("dbo.Appointments", "RoomID", "dbo.Appointments", "AppointmentID");
+            AddForeignKey("dbo.Appointments", "PatientID", "dbo.Appointments", "AppointmentID");
+            AddForeignKey("dbo.Appointments", "DoctorID", "dbo.Appointments", "AppointmentID");
+        }
+    }
+}
