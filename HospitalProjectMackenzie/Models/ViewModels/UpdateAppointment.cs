5d7e5a77feabb0c4e7ded09aa7b8ad99b15ebcb1
Stephen-Heff <folosstephen@gmail.com>
2023-03-27 23:21:26 -0400

    added controller/views for patients and appointments

---
+++ HospitalProjectMackenzie/Models/ViewModels/UpdateAppointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProjectMackenzie.Models.ViewModels
+{
+	public class UpdateAppointment
+	{
+		public AppointmentDto SelectedAppointment { get; set; }
+	}
+}
